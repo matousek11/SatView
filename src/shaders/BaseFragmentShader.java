f6db83c9f7c070d6dd4e1fa0503c91346829a386
Lukas Matousek <matoulu1@uhk.cz>
2025-04-23 14:21:40 +0200

    feat: shaders

--- src/shaders/BaseFragmentShader.java
+++ src/shaders/BaseFragmentShader.java
@@ -26,11 +26,12 @@ public class BaseFragmentShader implements Shaderable {
         return """
             #version 330 core
         
-            in vec3 vertexColor;
             out vec4 FragColor;
+            
+            uniform vec3 color;
         
             void main() {
-                FragColor = vec4(vertexColor, 1.0);;
+                FragColor = vec4(color, 1.0);
             }
         """;
     }
